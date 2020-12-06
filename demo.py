985ce811adf3e7f66330de715a6fa19126472bb4
n00bmaster68 <63935745+n00bmaster68@users.noreply.github.com>
2020-12-06 16:59:11 +0700

    first commit

---
+++ demo.py
@@ -0,0 +1,14 @@
+from download_image import download_image, get_url
+from crawl_titles import get_titles_on_web
+from natural_language_processing import get_name_of_object_in_image
+
+if __name__ == "__main__":
+	filename = "test.jpg"
+	url = get_url(filename)
+	titles = get_titles_on_web(filename)
+	title = get_name_of_object_in_image(titles)
+
+	try:
+		download_image(title)
+	except Exception:
+		print("Could not crawl")
