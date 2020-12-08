21e73fd08af1562ffb704c671a6cf0c1c907ef8c
n00bmaster68 <63935745+n00bmaster68@users.noreply.github.com>
2020-12-08 21:39:26 +0700

    edit

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ from crawl_titles import get_titles_on_web
 from natural_language_processing import get_name_of_object_in_image
 
 if __name__ == "__main__":
-	filename = "test.jpg"
-	titles = get_titles_on_web(filename)
-	title = get_name_of_object_in_image(titles)
+	filename= "test.jpg"
+	titles  = get_titles_on_web(filename)
+	title   = get_name_of_object_in_image(titles)
 
 	try:
 		download_image(title)
